45061dbfb131c86eaf39d3ae8d98d36abec51db0
DanyloLNU <danylo.tsebro@lnu.edu.ua>
2023-11-13 22:54:27 +0200

    created product test model

--- kurs/dashboard/models.py
+++ kurs/dashboard/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY = (
+    ('Верхній одяг', 'Верхній одяг'),
+    ('Взуття', 'Взуття'),
+    ('Штани', 'Штани'),
+    ('Категорія', 'Категорія'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=50, null=True, choices=CATEGORY)
+    quanity = models.PositiveIntegerField(null=True)
+
+
+    def __str__(self):
+        return f'{self.category}  {self.name} Кількість: {self.quanity}'
