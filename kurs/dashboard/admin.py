94d38e5ddcf6dba230b649fa611f8ce260d2ade8
DanyloLNU <danylo.tsebro@lnu.edu.ua>
2023-11-14 19:28:39 +0200

    order db, orderadmin class, meta classes

--- kurs/dashboard/admin.py
+++ kurs/dashboard/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 admin.site.site_header = 'Курсова робота'
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'quanity',)
-    list_filter = ('category', 'quanit')
+    list_filter = ('category', 'quanity')
+
+   
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('product', 'staff', 'date', 'order_quanity')
+
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
 
--- kurs/dashboard/models.py
+++ kurs/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -15,6 +16,22 @@ class Product(models.Model):
     category = models.CharField(max_length=50, null=True, choices=CATEGORY)
     quanity = models.PositiveIntegerField(null=True)
 
+    class Meta:
+        verbose_name_plural = 'Товар'
+     
 
     def __str__(self):
         return f'{self.category}  {self.name} Кількість: {self.quanity}'
+    
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_quanity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Замовлення'
+
+    def __str__(self):
+        return f'{self.product} замовив(-ла) {self.staff.username}'
