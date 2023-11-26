fdb62e77054ce8f5f8c65bec04198d5f76f5a58f
DanyloLNU <danylo.tsebro@lnu.edu.ua>
2023-11-26 18:53:24 +0200

    from sqlite to mysql

--- kurs/kurs/settings.py
+++ kurs/kurs/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'kurs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'kursova', 
+    'USER':'root', 
+    'PASSWORD':'admin',
+    'HOST':'localhost', 
+    'PORT':'3306', 
+}
 }
 
 
